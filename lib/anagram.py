e9006f9fba146a3aa6061fbccb0a5f9911c97ec6
kibetbrian <brian.kibet2@student.moringaschool.com>
2023-08-21 20:31:54 -0400

    completed lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, other_words):
+        anagram = []
+        for word in other_words:
+            if sorted(word.lower()) == sorted(self.word.lower()) and word.lower() != self.word.lower():
+                anagram.append(word)
+        return anagram
